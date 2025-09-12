215c8a20ca18d2176d43d50d93e1e7e5e423127c
dontchange365 <dontchange365@gmail.com>
2025-09-12 22:51:17 +0530

    Add files via upload

--- utils/avatarGenerator.js
+++ utils/avatarGenerator.js
@@ -1,7 +1,10 @@
 // utils/avatarGenerator.js
-function getDiceBearAvatarUrl(name, randomSeed = '') {
-    const seedName = (typeof name === 'string' && name) ? name.toLowerCase() : 'default_seed';
-    const seed = encodeURIComponent(seedName + randomSeed);
-    return `https://api.dicebear.com/8.x/adventurer/svg?seed=${seed}&flip=true&radius=50&doodle=true&scale=90`;
+
+const { avatarUrls } = require('../services/cloudinaryAvatars');
+
+function getRandomCloudinaryAvatarUrl() {
+    const randomIndex = Math.floor(Math.random() * avatarUrls.length);
+    return avatarUrls[randomIndex];
 }
-module.exports = { getDiceBearAvatarUrl };
+
+module.exports = { getRandomCloudinaryAvatarUrl };
